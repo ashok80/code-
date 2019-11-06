6baf6840face66cadf1b0027d00ab059b051164b
naresh kumar jaggi <botdigit@outlook.com>
2019-11-06 20:28:35 +0530

    celery worker programatic implementation

--- MyPro/__init__.py
+++ MyPro/__init__.py
@@ -1,1 +1,13 @@
+# -*- coding: utf-8 -*-
+
 from MyPro.celery import app as celery_app
+import threading
+
+__all__ = ('celery_app',)
+
+def file_main():
+    print("running celery app")
+    celery_app.start(['celery', '-A', 'MyPro', 'beat'])
+
+x = threading.Thread(target=file_main, daemon=True)
+x.start()
